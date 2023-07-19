ec30a46e28003b74be2acbfdcd79554eccf7efe8
adnan <TheMuhammadAdnan99@gmail.com>
2023-07-19 15:06:28 +0500

    data excel sheet not show

--- project.py
+++ project.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+#To save the product details list to an Excel sheet
+import pandas as pd
 
 # Create a list to store product details
 products_list = []
@@ -56,21 +58,35 @@ products = driver.find_elements(By.XPATH, '//*[@id="campaign-1689620400000"]/div
 
 # Iterate over each product element and extract the details
 for product in products:
-    product_name = product.find_element("class name", "sale-title").text
-    product_price = product.find_element("class name", "sale-price").text
-    product_originprice = product.find_element("class name", "origin-price-value").text
-    product_discount = product.find_element("class name", "discount").text
-# Store product information in the dictionary
-    products_dict[product_name] = {
+    product_name = product.find_element(By.CLASS_NAME, "sale-title").text
+    product_price = product.find_element(By.CLASS_NAME, "sale-price").text
+    product_originprice = product.find_element(By.CLASS_NAME, "origin-price-value").text
+    product_discount = product.find_element(By.CLASS_NAME, "discount").text
+
+    # Store product information in a dictionary
+    product_details = {
+        "name": product_name,
         "price": product_price,
         "origin_price": product_originprice,
-        "discount": product_discount}
-    
-     # Append the product details dictionary to the products_list
-    products_list.append(products_dict)
+        "discount": product_discount
+    }
 
-# Print the dictionary containing the product details
-print(products_dict)
+    # Append the product details dictionary to the products_list
+    products_list.append(product_details)
+
+# Convert the products_list to a pandas DataFrame
+df = pd.DataFrame(products_list)
+
+# Print the list containing the product details
+print('output:', products_list)
+
+# Define the file name for the Excel sheet
+excel_file = "product_details.xlsx"
+
+# Save the DataFrame to an Excel file
+df.to_excel(excel_file, index=False)
+
+print("Product details saved to", excel_file)
 
 # Close the browser
 driver.quit()
